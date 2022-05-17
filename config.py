a376b737af593eefd251b0571453f4b55ebefc15
DenisMuga <denismugah5@gmail.com>
2022-05-17 15:20:56 +0300

    deployment to heroku

--- config.py
+++ config.py
@@ -15,8 +15,7 @@ class Config:
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 class ProdConfig(Config):
-    # SQLALCHEMY_DATABASE_URI=os.environ.get("DATABASE_URL").replace('postgres://', 'postgresql://')
-    pass
+    SQLALCHEMY_DATABASE_URI=os.environ.get("DATABASE_URL").replace('postgres://', 'postgresql://')
 class DevConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get("DEV_DATABASE_URL")
     DEBUG = True
