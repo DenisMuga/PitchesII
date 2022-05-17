109ab25ab0b807d33086c02573699691f191d166
DenisMuga <denismugah5@gmail.com>
2022-05-17 03:41:07 +0300

    Importing mail, migrate, and login manager from flask to root app

--- app/__init__.py
+++ app/__init__.py
@@ -2,3 +2,6 @@ from flask import Flask
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstraps import Bootstrap  
+from flask_mail import Mail
+from flask_migrate import Migrate
+from flask_login import LoginManager
