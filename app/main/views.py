9c185dc8804a9fdb94da7cd36ebe43e528b3174c
DenisMuga <denismugah5@gmail.com>
2022-05-17 04:08:47 +0300

    Creating a route for register in views

--- app/main/views.py
+++ app/main/views.py
@@ -4,3 +4,18 @@ from flask_login import login_user,logout_user,login_required,current_user
 from ..models import User
 from flask import redirect,render_template,url_for,flash,request
 from .. import db
+
+
+@auth.route('/register', methods = ['POST','GET'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user=User.query.filter_by(username=form.username.data).first()
+        if user is None:
+            user = User(username =form.username.data, email = form.email.data,password=form.password1.data)
+            # user.set_password(form.password1.data)
+            user.save()
+            return redirect(url_for('auth.login'))
+        else:
+            flash('That username is in use try a new one')
+    return render_template('auth/signup.html', form=form)
