8e9df23e4e99312e1a3a41399db2d88f3e66098a
DenisMuga <denismugah5@gmail.com>
2022-05-17 04:11:17 +0300

    Creating a login route in views

--- app/main/views.py
+++ app/main/views.py
@@ -32,3 +32,10 @@ def login():
             return redirect(next or url_for('main.index'))
         flash('Invalid email address or Password.')    
     return render_template('auth/login.html', form=form)
+
+
+@auth.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('main.index'))
