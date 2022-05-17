056872e7a462e9851e333adfa2ee9faca3b7d47c
DenisMuga <denismugah5@gmail.com>
2022-05-17 04:09:54 +0300

    Creating a login route in views

--- app/main/views.py
+++ app/main/views.py
@@ -19,3 +19,16 @@ def register():
         else:
             flash('That username is in use try a new one')
     return render_template('auth/signup.html', form=form)
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email = form.email.data).first()
+        if user != None and user.verify_password(form.password.data):
+            login_user(user, form.remember.data)
+            next = request.args.get("next")
+            return redirect(next or url_for('main.index'))
+        flash('Invalid email address or Password.')    
+    return render_template('auth/login.html', form=form)
