0a42d4dc5f2a85fbfc057de5af647544c6c29e1f
DenisMuga <denismugah5@gmail.com>
2022-05-17 03:46:01 +0300

    Instantiating various import variables

--- app/__init__.py
+++ app/__init__.py
@@ -5,3 +5,12 @@ from flask_bootstraps import Bootstrap
 from flask_mail import Mail
 from flask_migrate import Migrate
 from flask_login import LoginManager
+
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+db = SQLAlchemy()
+from app.models import User,Blog,Comment,Subscriber
+mail = Mail()
+bootstap = Bootstrap()
